b2a0d107aae1add5a80055a52fd510c3861cca3d
fad16papa <fad16papa@gmail.com>
2020-07-18 00:07:15 +0800

    Commit changes in GameOptionsModel

--- RockPaperScissors/Models/GameOptionsModel.cs
+++ RockPaperScissors/Models/GameOptionsModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,31 @@ namespace RockPaperScissors.Models
 {
     public class GameOptionsModel
     {
+        [DisplayName("Computer Wins")]
         public int ComputerWins { get; set; }
+
+        [DisplayName("First COmputer Wins")]
         public int FirstComputerWins { get; set; }
+
+        [DisplayName("Second COmputer Wins")]
         public int SecondComputerWins { get; set; }
+
+        [DisplayName("Player Wins")]
         public int PlayerWins { get; set; }
+
+        [DisplayName("Player Name")]
         public string PlayerName { get; set; }
-        public int ScoreX { get; set; }
-        public int ScoreY { get; set; }
+
+        [DisplayName("Score X")]
+        public int ScoreXAxis { get; set; }
+
+        [DisplayName("Score Y")]
+        public int ScoreYAxis { get; set; }
+
+        [DisplayName("Number of Games")]
         public int NumberOfGames { get; set; }
+
+        [DisplayName("Another Round")]
         public bool AnotherRound { get; set; }
     }
 }
